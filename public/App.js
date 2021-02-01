45f2d4e607c1a51afc5cfde6133c0406d9741fab
VaidehiM-sys <52694641+VaidehiM-sys@users.noreply.github.com>
2021-01-31 23:41:05 -0800

    Changes Commited

--- public/App.js
+++ public/App.js
@@ -1,22 +1,36 @@
-var contentNode = document.getElementById('contents');
-var component = /*#__PURE__*/React.createElement("div", {
-  className: "grid-container"
+const contentNode = document.getElementById('contents');
+const component = /*#__PURE__*/React.createElement("section", {
+  id: "hero_section",
+  className: "top_cont_outer"
+}, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", {
+  className: "card hovercard"
 }, /*#__PURE__*/React.createElement("div", {
-  className: "item1"
-}, /*#__PURE__*/React.createElement("h1", null, " Vaidehi Mungekar ")), /*#__PURE__*/React.createElement("div", {
-  className: "item2"
+  className: "cardheader"
+}), /*#__PURE__*/React.createElement("div", {
+  className: "avatar"
 }, /*#__PURE__*/React.createElement("img", {
-  src: "./img/profile-pic.png",
-  alt: "Profile Picture",
-  width: "200",
-  height: "200"
+  alt: "",
+  src: "img/profile-pic.png"
 })), /*#__PURE__*/React.createElement("div", {
-  className: "item3"
-}, "I am an enthusiastic tech professional currently pursuing M.S. in Computer Science student at San Diego State University. I have complete understanding of software development lifecycle from design, implementation to the deployment of the product using Agile methodology."), /*#__PURE__*/React.createElement("div", {
-  className: "item4"
+  className: "info"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "title"
 }, /*#__PURE__*/React.createElement("a", {
-  href: "https://github.com/VaidehiM-sys",
   target: "_blank",
-  className: "git-repo"
-}, "VIEW MY GITHUB REPO")));
+  href: "https://www.linkedin.com/in/vaidehi-mungekar/"
+}, "Vaidehi Mungekar")), /*#__PURE__*/React.createElement("div", {
+  className: "desc"
+}, /*#__PURE__*/React.createElement("b", null, "M.S. in Computer Science student at San Diego State University")), /*#__PURE__*/React.createElement("div", {
+  className: "desc"
+}, /*#__PURE__*/React.createElement("h6", null, "I am an enthusiastic tech professional with complete understanding of software development lifecycle", /*#__PURE__*/React.createElement("br", null), "from design, implementation to the deployment of the product using Agile methodology."))), /*#__PURE__*/React.createElement("div", {
+  className: "bottom"
+}, /*#__PURE__*/React.createElement("div", {
+  id: "filters"
+}, /*#__PURE__*/React.createElement("ul", {
+  className: "clearfix"
+}, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
+  className: "",
+  href: "https://github.com/VaidehiM-sys",
+  target: "_blank"
+}, /*#__PURE__*/React.createElement("h5", null, "VIEW MY GITHUB REPO")))))))));
 ReactDOM.render(component, contentNode);
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-var contentNode = document.getElementById('contents');
-var component = <div className="grid-container">
-    <div className="item1">
-      <h1> Vaidehi Mungekar </h1>
-    </div>
-    <div className="item2">
-      <img src= "./img/profile-pic.png" alt="Profile Picture" width="200" height="200"></img>
-    </div>
-    <div className="item3"> 
-    I am an enthusiastic tech professional currently pursuing M.S. in Computer Science student at San Diego State University.
-    I have complete understanding of software development lifecycle 
-    from design, implementation to the deployment of the product using Agile methodology.
-    </div>
-    <div className="item4">
-      <a href="https://github.com/VaidehiM-sys" target = "_blank" className="git-repo">VIEW MY GITHUB REPO</a>
-    </div>
-  </div>;
+const contentNode = document.getElementById('contents');
+const component = (<section id="hero_section" className="top_cont_outer">
+	                    <div>  
+		                    <div className="card hovercard">
+			                    <div className="cardheader"></div>
+			                    <div className="avatar">
+				                    <img alt="" src="img/profile-pic.png"/>
+			                    </div>
+			                    <div className="info">	
+				                    <div className="title">
+					                    <a target="_blank" href="https://www.linkedin.com/in/vaidehi-mungekar/">Vaidehi Mungekar</a>
+				                    </div>
+				                    <div className="desc"><b>M.S. in Computer Science student at San Diego State University</b></div>
+				                    <div className="desc">
+					                    <h6>I am an enthusiastic tech professional with complete understanding of software development lifecycle 
+					                    <br />from design, implementation to the deployment of the product using Agile methodology.</h6>
+				                    </div>
+			                    </div>
+			                    <div className="bottom">         
+				                    <div id="filters">
+					                    <ul className="clearfix">
+						                    <li><a className="" href="https://github.com/VaidehiM-sys" target="_blank">
+						                    <h5>VIEW MY GITHUB REPO</h5>
+						                    </a></li>
+					                    </ul>
+				                    </div> 
+			                    </div>
+		                    </div>
+	                    </div>
+</section>);
 ReactDOM.render(component, contentNode);
+
 
